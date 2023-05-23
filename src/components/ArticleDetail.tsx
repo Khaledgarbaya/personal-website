8ed9bfbd84a97871673d0d21b89aabac0e0f0125
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-05-23 11:27:24 +0200

    fix(style): Add syntax higlight

--- src/components/ArticleDetail.tsx
+++ src/components/ArticleDetail.tsx
@@ -2,6 +2,24 @@ import { registerUniformComponent } from "@uniformdev/canvas-react";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import rangeParser from "parse-numeric-range";
+
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
+
 type BioPropsType = {
   title: string;
   content: string;
@@ -9,6 +27,49 @@ type BioPropsType = {
 };
 
 export function ArticleDetail({ title, content, publishDate }: BioPropsType) {
+  const syntaxTheme = oneDark;
+
+  const MarkdownComponents: object = {
+    code({ node, inline, className, ...props }) {
+      const hasLang = /language-(\w+)/.exec(className || "");
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights: object = (applyHighlights: number) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, "");
+          const strlineNumbers = RE?.test(metadata)
+            ? RE?.exec(metadata)[1]
+            : "0";
+          const highlightLines = rangeParser(strlineNumbers);
+          const highlight = highlightLines;
+          const data: string = highlight.includes(applyHighlights)
+            ? "highlight"
+            : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return hasLang ? (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={hasLang[1]}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={true}
+          wrapLines={hasMeta}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+        >
+          {props.children}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props} />
+      );
+    },
+  };
   return (
     <div className="mx-auto max-w-2xl lg:max-w-5xl">
       <article>
@@ -24,8 +85,13 @@ export function ArticleDetail({ title, content, publishDate }: BioPropsType) {
             <span className="ml-3">published at {publishDate}</span>
           </time>
         </header>
-        <div className="prose prose-xl mt-3">
-          <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
+        <div className="prose prose-xl mt-3 prose-pre:bg-transparent">
+          <ReactMarkdown
+            components={MarkdownComponents}
+            rehypePlugins={[rehypeRaw]}
+          >
+            {content}
+          </ReactMarkdown>
         </div>
       </article>
     </div>
