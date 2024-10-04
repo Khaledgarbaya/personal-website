f44f69f4ad69b88a491af5bf1dd038109786f39c
Khaled Garbaya <khaledgarbaya@gmail.com>
2024-10-04 11:52:26 +0200

    fix newsletter form

--- app/components/newsletter-form.tsx
+++ app/components/newsletter-form.tsx
@@ -31,8 +31,10 @@ export default function NewsletterForm() {
       </p>
       <div className="mt-6 flex">
         <input
-          placeholder="Email address"
-          aria-label="Email address"
+          id="email_address"
+          name="email_address"
+          aria-label="Email Address"
+          placeholder="Email Address"
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
           type="email"
